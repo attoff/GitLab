5f0bc7db23154024f452f057c3b2c5def75f1b6c
NilssonJohan <johan.skolarbeten@gmail.com>
2016-12-02 10:48:56 +0100

    test1 commit

--- src/MyNode.java
+++ src/MyNode.java
@@ -12,7 +12,7 @@ public class MyNode {
   private MyNode left;
   private MyNode right;
   private int value;
-
+	TEST 1 förändring
   /**
    * Class constructor initializing this node's value.
    * 
